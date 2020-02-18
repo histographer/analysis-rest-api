4d923e1411ef9bd27882680009685eae63ca4623
Oda Omholt <oda.omh@gmail.com>
2020-02-18 10:08:17 +0100

    refactor: change project name from analysis_rest_api to api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-"""analysis_rest_api URL Configuration
+"""api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
