eff2c5f5719d7635f0e7d7345121762fc8ba3ff3
Oda Omholt <oda.omh@gmail.com>
2020-02-21 12:46:03 +0100

    feat: add functioning endpoint for suggesting new pair

--- ranking/views.py
+++ ranking/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.decorators import parser_classes
+from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from .ranking import random_pair
 
-# Create your views here.
+
+@api_view(['POST'])
+@parser_classes([JSONParser])
+#@renderer_classes([JSONRenderer])
+#@authentication_classes(...)
+def suggest_pair(request):
+    """
+    Suggests a new pair for comparison. Currently returns a random pair.
+    """
+    number_of_items = request.data.get("number_of_items")
+    comparison_data = request.data.get("comparison_data")
+    pairs = []
+    for item in comparison_data:
+        if item == 'id':
+            pass
+        chosen = item.get('chosen').get('id')
+        other = item.get('other').get('id')
+        pair = (chosen, other)
+        pairs.append(pair)
+    pair = random_pair(number_of_items)
+    response = {'left': {'id': pair[0]}, 'right': {'id': pair[1]}}
+    return Response(response)
