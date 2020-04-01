8bc4dc98ae2565a587426ed9bd83e7672542080a
Oda Omholt <oda.omh@gmail.com>
2020-04-01 18:09:52 +0200

    chore: update /analyze

--- analysis/views.py
+++ analysis/views.py
@@ -1,6 +1,11 @@
+from .analysis import do_analysis
+
 from rest_framework.decorators import parser_classes, api_view
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
+import threading
+from yaml import safe_load
+
 
 HE_ANALYSIS, RGB_ANALYSIS = "he", "rgb"
 
@@ -12,13 +17,20 @@ def analyze(request):
     Takes in an annotation group and the analyses to be performed on it,
     and starts the analyses.
     """
-    group_id = request.data.get("group_id")
-    annotation_ids = request.data.get("annotations")
-    analyses = request.data.get("analysis")
-    if HE_ANALYSIS in analyses:
-        pass
-        # start_he_analysis(group_id, annotation_ids)
-    if RGB_ANALYSIS in analyses:
-        pass
-        # start_rgb_analysis(group_id, annotation_ids)
-    return Response(status=202)
+    with open('secrets.yml', 'r') as f:
+        # TODO: Enter correct secrets
+        host_info = safe_load(f)['host']
+    try:
+        analysis_id = request.data.get("analysisId")
+        annotation_ids = request.data.get("annotations")
+        analysis_names = request.data.get("analysis")
+        args = (host_info, annotation_ids, analysis_names)
+        analysis_thread = threading.Thread(target=do_analysis, args=args)
+        return Response(status=202)
+    except AttributeError as e:
+        print(e)
+        return Response(data=e, status=400)
+
+
+
+
