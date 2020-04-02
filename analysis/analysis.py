1891388694389c3abe244f56832523b211dee29c
Oda Omholt <oda.omh@gmail.com>
2020-04-02 19:33:30 +0200

    feat: halfway implementation of separate project_id

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -5,15 +5,16 @@ from .encode import dict_list_to_csv_b64
 import os
 import traceback
 
+# TODO: Create proper setup for local/prod,
 WIZARD_BACKEND_URL = os.getenv("WIZARD_BACKEND_URL")
+if not WIZARD_BACKEND_URL:
+    WIZARD_BACKEND_URL = "http://example.com"
 
 
-def do_analysis(analysis_id, annotation_ids, analysis_names, host_info):
-    args = (host_info, annotation_ids, analysis_names)
+def do_analysis(project_id, analysis_id, annotation_ids, analysis_names, host_info):
     try:
-        # TODO: WIZARD_BACKEND_URL as environment variable in .env
-        # url = f"{WIZARD_BACKEND_URL}/analysisResults"
-        results = image_analysis(host_info, annotation_ids, analysis_names)
+        url = f"{WIZARD_BACKEND_URL}/analysisResults"
+        results = image_analysis(host_info, annotation_ids, analysis_names)  # TODO: image_analysis(project_id, ...)
         csv_b64 = dict_list_to_csv_b64(results).decode()
         data = {
             "csvBase64": csv_b64,
@@ -21,12 +22,19 @@ def do_analysis(analysis_id, annotation_ids, analysis_names, host_info):
             "annotations": results
         }
         print(data)
-    #    response = requests.post(url, data=json.dumps(data))
+        response = requests.post(url, data=json.dumps(data))
     except Exception:
         # TODO: More sophisticated error handling/logging?
         print(f"Exception: {traceback.format_exc()}")
-
-        # url = f"{WIZARD_BACKEND_URL}/analysisInfo" TODO: Check that this endpoint is correct
-        data = {"status": "failure"}
+        url = f"{WIZARD_BACKEND_URL}/??"  # TODO: Add correct endpoint for failure feedback
+        data = {
+            "analysisId": analysis_id,
+            "status": "failure"
+        }
         print(data)
-        # response = requests.post(url, data=json.dumps(data)
+        response = requests.post(url, data=json.dumps(data))
+    # TODO: Error handling/raising if unable to send request to middleware?
+    if response.status_code == 200:
+        print("success")
+    else:
+        pass
