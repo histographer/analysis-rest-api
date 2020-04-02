78ce0515cba48344898174f7c713aa3b81cf5a03
Oda Omholt <oda.omh@gmail.com>
2020-04-02 18:35:53 +0200

    chore: start to set up support for running local or prod

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,25 +1,32 @@
 from histographer.analysis.image.high_level import image_analysis
 import json
 import requests
-from analysis.encode import dict_list_to_csv_b64
+from .encode import dict_list_to_csv_b64
+import os
+import traceback
+
+WIZARD_BACKEND_URL = os.getenv("WIZARD_BACKEND_URL")
 
 
 def do_analysis(analysis_id, annotation_ids, analysis_names, host_info):
-    print(analysis_id, annotation_ids, analysis_names, host_info)
-    #url = "http://??.digipat.no/analysisResults"
     args = (host_info, annotation_ids, analysis_names)
     try:
+        # TODO: WIZARD_BACKEND_URL as environment variable in .env
+        # url = f"{WIZARD_BACKEND_URL}/analysisResults"
         results = image_analysis(host_info, annotation_ids, analysis_names)
-        csv_b64 = dict_list_to_csv_b64(results)
-        print(results)
-    #    # TODO: Send b64 encoded CSV
-    #    response = requests.post(url, data=json.dumps(results))
-    except ConnectionError as e:
-        print(e)
-    #    # TODO: Send different request to a different URL, details will be declared later
-    #    response = requests.post(url, data="502 – Error during analysis.")
-    # TODO: Other errors we should handle, or just have a generic except instead?
+        csv_b64 = dict_list_to_csv_b64(results).decode()
+        data = {
+            "csvBase64": csv_b64,
+            "analysisId": analysis_id,
+            "annotations": results
+        }
+        print(data)
+    #    response = requests.post(url, data=json.dumps(data))
+    except Exception:
+        # TODO: More sophisticated error handling/logging?
+        print(f"Exception: {traceback.format_exc()}")
 
-
-
-
+        # url = f"{WIZARD_BACKEND_URL}/analysisInfo" TODO: Check that this endpoint is correct
+        data = {"status": "failure"}
+        print(data)
+        # response = requests.post(url, data=json.dumps(data)
