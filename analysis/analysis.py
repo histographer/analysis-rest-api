42c0a9a1fb17a8681824858c96859fc3d0dbf50c
Oda Omholt <oda.omh@gmail.com>
2020-04-02 14:18:18 +0200

    feat: enable image analysis through endpoint

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -5,16 +5,19 @@ from analysis.encode import dict_list_to_csv_b64
 
 
 def do_analysis(analysis_id, annotation_ids, analysis_names, host_info):
-    url = "http://digipat.no/analysisResults"
+    print(analysis_id, annotation_ids, analysis_names, host_info)
+    #url = "http://??.digipat.no/analysisResults"
     args = (host_info, annotation_ids, analysis_names)
     try:
-        results = image_analysis(analysis_id, annotation_ids, analysis_names, host_info)
+        results = image_analysis(host_info, annotation_ids, analysis_names)
         csv_b64 = dict_list_to_csv_b64(results)
-        # TODO: Send b64 encoded CSV
-        response = requests.post(url, data=json.dumps(results))
+        print(results)
+    #    # TODO: Send b64 encoded CSV
+    #    response = requests.post(url, data=json.dumps(results))
     except ConnectionError as e:
-        # TODO: Send different request to a different URL, details will be declared later
-        response = requests.post(url, data="502 – Error during analysis.")
+        print(e)
+    #    # TODO: Send different request to a different URL, details will be declared later
+    #    response = requests.post(url, data="502 – Error during analysis.")
     # TODO: Other errors we should handle, or just have a generic except instead?
 
 
