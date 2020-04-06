94647ce40a060c633f387996b7a04ad92df2c935
Oda Omholt <oda.omh@gmail.com>
2020-04-06 09:10:54 +0200

    fix: update with callback urls

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,18 +2,12 @@ from histographer.analysis.image.high_level import image_analysis
 import json
 import requests
 from .encode import dict_list_to_csv, recursive_flatten
-import os
 import traceback
 
-# TODO: Create proper setup for local/prod:
-WIZARD_BACKEND_URL = os.getenv("WIZARD_BACKEND_URL")
-if not WIZARD_BACKEND_URL:
-    WIZARD_BACKEND_URL = "http://example.com"
 
-
-def do_analysis(project_id, analysis_id, annotation_ids, analysis_names, host_info):
+def do_analysis(project_id, analysis_id, annotation_ids, analysis_names, host_info, analysis_results_url, update_status_url):
     try:
-        url = f"{WIZARD_BACKEND_URL}/analysisResults"
+        url = f"{analysis_results_url}"
         results = image_analysis(host_info, annotation_ids, analysis_names)  # TODO: image_analysis(project_id, ...)
         csv = dict_list_to_csv(recursive_flatten(results))
         data = {
@@ -26,7 +20,7 @@ def do_analysis(project_id, analysis_id, annotation_ids, analysis_names, host_in
     except Exception:
         # TODO: More sophisticated error handling/logging?
         print(f"Exception: {traceback.format_exc()}")
-        url = f"{WIZARD_BACKEND_URL}/analysisInformation"
+        url = f"{update_status_url}"
         data = {
             "analysisId": analysis_id,
             "status": "failure"
